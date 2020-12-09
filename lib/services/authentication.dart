9a72273399d89abc37947169529307e66d0bcb06
mc-off <makov.tm@gmail.com>
2020-12-09 16:44:16 +0300

    Login screen with saving user to app cache files.

---
+++ lib/services/authentication.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:greenapp/models/user.dart';
+import 'package:greenapp/services/http_auth.dart';
+
+import 'base_auth.dart';
+
+class Auth implements BaseAuth {
+  final HttpAuth _httpAuth = HttpAuth();
+
+  Future<User> getCurrentUser() async {
+    User user = await _httpAuth.currentUser();
+    return user;
+  }
+
+//  Future<bool> isEmailVerified() {
+//    // TODO: implement isEmailVerified
+//    throw UnimplementedError();
+//  }
+
+//  @override
+//  Future<void> sendEmailVerification() {
+//    FirebaseUser user = await _firebaseAuth.currentUser();
+//    user.sendEmailVerification();
+//  }
+
+  Future<String> signIn(String email, String password) async {
+    User user = await _httpAuth.signInWithEmailAndPassword(email, password);
+    return user.token;
+  }
+
+//Future<void> signOut() async {
+//    return _firebaseAuth.signOut();
+//  }
+
+  Future<String> signUp(String email, String password) async {
+    User user = await _httpAuth.signUpWithEmailAndPassword(email, password);
+    return user.token;
+  }
+}
